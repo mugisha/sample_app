55dcfae9d66cd39d55fed7f7419a67ab76a12de0
Joseph Mugisha <demugisha@gmail.com>
2012-05-04 06:39:13 -0700

    added tests for titles of Home, About and Help pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,23 +2,41 @@ require 'spec_helper'
 
 describe "Static pages" do
   describe "Home page" do
-    it "should have the content 'Sample App'" do
+    it "should have h1 'Sample App'" do
       	visit '/static_pages/home'
-	page.should have_content('Sample App')
+	page.should have_selector('h1' :text=>'Sample App')
     end
+
+    it "should have the title 'Home'" do
+       visit '/static_pages/home'
+       page.should have_selector('title',
+			:text => "Ruby on Rails Tutorial Sample App | Home")
+     end		
   end
 
   describe "Help page" do
-      it "should have the content 'Help'" do
+      it "should have h1 'Help'" do
 	visit '/static_pages/help'
-	page.should have_content('Help')
+	page.should have_selector('h1' :text=> 'Help')
       end
+
+    it "should have the title 'Help'" do 
+	visit "/static_pages/help"
+	page.should have_selector('title',
+			:text => " | Help")
+    end
    end
 
   describe "About page" do
-     it "should have the content 'About us' " do
+     it "should have h1 'About us' " do
 	visit '/static_pages/about'
-	page.should have_content('About us')
-     end 
+	page.should have_selector('h1'=>'About us')
+     end
+
+     it "should have the title 'About'" do
+	visit "/static_pages/about"
+	page.should have_selector('title',
+			:text => " | About")
+     end
    end
 end
