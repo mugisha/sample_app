837e6c4e522563d29e0bab3756f739a52bf644b9
Joseph Mugisha <demugisha@gmail.com>
2012-05-12 19:22:27 -0700

    implemented named routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,18 +3,18 @@ require 'spec_helper'
 describe "Static pages" do
   describe "Home page" do
     it "should have h1 'Sample App'" do
-      	visit '/static_pages/home'
+      	visit root_path
 	page.should have_selector('h1', :text=>'Sample App')
     end
 
     it "should have the base title'" do
-       visit '/static_pages/home'
+       visit root_path
        page.should have_selector('title',
 			:text => "Ruby on Rails Tutorial Sample App")
      end
 
      it "should not have a custome page title" do
-	visit '/static_pages/home'
+	visit root_path
 	page.should_not have_selector('title',:text => '|Home')
       end
 		
@@ -22,12 +22,12 @@ describe "Static pages" do
 
   describe "Help page" do
       it "should have h1 'Help'" do
-	visit '/static_pages/help'
+	visit help_path
 	page.should have_selector('h1', :text=> 'Help')
       end
 
     it "should have the title 'Help'" do 
-	visit "/static_pages/help"
+	visit help_path
 	page.should have_selector('title',
 			:text => " | Help")
     end
@@ -35,14 +35,22 @@ describe "Static pages" do
 
   describe "About page" do
      it "should have h1 'About us' " do
-	visit '/static_pages/about'
+	visit about_path
 	page.should have_selector('h1', :text=>'About us')
      end
 
      it "should have the title 'About'" do
-	visit "/static_pages/about"
+	visit about_path
 	page.should have_selector('title',
 			:text => " | About")
      end
+   end
+
+  describe "Contact page" do
+      it "should have the h1 'Contact' " do
+	  visit contact_path
+	  page.should have_selector('title',
+				text: "Ruby on Rails Tutorial Sample App | Contact")
+      end
    end
 end
