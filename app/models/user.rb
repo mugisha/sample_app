491674cad10ac9360aeec8ac918d354becb8cbb5
Joseph Mugisha <demugisha@gmail.com>
2012-06-07 05:20:58 -0700

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,4 +26,10 @@ class User < ActiveRecord::Base
 
   before_save { |user| user.email = email.downcase}
 
+  before_save :create_remember_token
+
+  private 
+  def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+  end
 end
